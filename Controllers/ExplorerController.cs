77fc63e12e9b5667b0ff28ec296a82ed34d470b4
Clint.Network <me@clint.network>
2019-03-25 00:00:38 +0100

    Implement ExplorerController with cache

--- Controllers/ExplorerController.cs
+++ Controllers/ExplorerController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using RestSharp;
 
 namespace StratisExplorerApi.Controllers
 {
@@ -11,25 +13,65 @@ namespace StratisExplorerApi.Controllers
     [ApiController]
     public class ExplorerController : ControllerBase
     {
+        private readonly IMemoryCache _memoryCache;
+
+        public ExplorerController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        
         [HttpGet]
         [Route("block/hash/{hash}")]
-        public ActionResult<object> BlockHash(string hash)
+        public async Task<ActionResult<object>> BlockHashAsync(string hash)
         {
-            return "ok";
+            string lastResponse;
+
+            if (!_memoryCache.TryGetValue($"hash[{hash}]", out lastResponse))
+            {
+                var client = new RestClient($"https://stratis.guru/api/block/{hash}");
+                var request = new RestRequest(Method.GET);
+                lastResponse = (await client.ExecuteTaskAsync(request)).Content;
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromDays(1));
+                _memoryCache.Set($"hash[{hash}]", lastResponse, cacheEntryOptions);
+            }
+
+            return lastResponse;
         }
 
         [HttpGet]
         [Route("address/{address}")]
-        public ActionResult<object> Address(string address)
+        public async Task<ActionResult<object>> AddressAsync(string address)
         {
-            return "ok";
+            string lastResponse;
+
+            if (!_memoryCache.TryGetValue($"address[{address}]", out lastResponse))
+            {
+                var client = new RestClient($"https://stratis.guru/api/address/{address}");
+                var request = new RestRequest(Method.GET);
+                lastResponse = (await client.ExecuteTaskAsync(request)).Content;
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromDays(1));
+                _memoryCache.Set($"address[{address}]", lastResponse, cacheEntryOptions);
+            }
+
+            return lastResponse;
         }
 
         [HttpGet]
         [Route("transaction/{transactionId}")]
-        public ActionResult<object> Transaction(string transactionId)
+        public async Task<ActionResult<object>> TransactionAsync(string transactionId)
         {
-            return "ok";
+            string lastResponse;
+
+            if (!_memoryCache.TryGetValue($"transaction[{transactionId}]", out lastResponse))
+            {
+                var client = new RestClient($"https://stratis.guru/api/transaction/{transactionId}");
+                var request = new RestRequest(Method.GET);
+                lastResponse = (await client.ExecuteTaskAsync(request)).Content;
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromDays(1));
+                _memoryCache.Set($"transaction[{transactionId}]", lastResponse, cacheEntryOptions);
+            }
+
+            return lastResponse;
         }
     }
 }
