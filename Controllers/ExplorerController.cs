334ff31f6a11ae802889d51d7fe6d0060256bc2f
ToniGalmes <tonigalmes98@gmail.com>
2019-04-28 17:23:57 +0200

    Return error if BlockHash > blockHeight

--- Controllers/ExplorerController.cs
+++ Controllers/ExplorerController.cs
@@ -28,13 +28,22 @@ namespace StratisExplorerApi.Controllers
 
             if (!_memoryCache.TryGetValue($"hash[{hash}]", out lastResponse))
             {
-                var client = new RestClient($"https://stratis.guru/api/block/{hash}");
+                string responseHeight;
+                var client = new RestClient($"https://stratis.guru/api/block-height");
                 var request = new RestRequest(Method.GET);
-                lastResponse = (await client.ExecuteTaskAsync(request)).Content;
-                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromDays(1));
-                _memoryCache.Set($"hash[{hash}]", lastResponse, cacheEntryOptions);
-            }
+                responseHeight = (await client.ExecuteTaskAsync(request)).Content;
 
+                if(Convert.ToInt32(hash) <= Convert.ToInt32(responseHeight))
+                {
+                    client = new RestClient($"https://stratis.guru/api/block/{hash}");
+                    request = new RestRequest(Method.GET);
+                    lastResponse = (await client.ExecuteTaskAsync(request)).Content;
+                
+                    var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromDays(1));
+                    _memoryCache.Set($"hash[{hash}]", lastResponse, cacheEntryOptions);
+                }else lastResponse = "Error, blockIdx not existent";
+                
+            }
             return lastResponse;
         }
 
